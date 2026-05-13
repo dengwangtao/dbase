29dfe89ef6159f41675285627d720b0c1d166317
dengwangtao <2237915833@qq.com>
2026-05-13 12:56:09 +0800

    feat(examples): 添加 ASIO 示例代码
    - 在 `CMakeLists.txt` 中添加了 `code/include/ext/asio` 的包含路径，以便能够正确编译引用 ASIO 库的代码。
    - 在 `asio_example.cpp` 中修改了 `#include` 语句，将 `"ext/asio/asio.hpp"` 修改为 `"asio.hpp"`，以匹配新的包含路径。
    - 在 `asio_example.cpp` 中添加了一个完整的 ASIO 示例代码，该代码展示了如何使用 ASIO 进行简单的 HTTP GET 请求，并读取响应数据。代码中使用了 `asio::io_context`、`tcp::resolver` 和 `tcp::socket` 进行网络通信，并通过 `asio::write` 发送请求，通过 `asio::read_some` 读取响应数据，最后将响应数据输出到标准输出。
    
    Closes #123

--- examples/asio_example.cpp
+++ examples/asio_example.cpp
@@ -1,10 +1,11 @@
 
-#include "ext/asio/asio.hpp"
+#include "asio.hpp"
 #include "dbase/log/log.h"
 #include <iostream>
 
 using asio::ip::tcp;
 
+/*
 int main()
 {
     asio::io_context io;
@@ -22,4 +23,44 @@ int main()
 
         DBASE_LOG_INFO("client connected: {}", socket.remote_endpoint().address().to_string());
     }
+}
+*/
+
+int main()
+{
+    asio::io_context ioc;
+
+    tcp::resolver resolver(ioc);
+    auto endpoints = resolver.resolve("www.bilibili.com", "http");
+
+    tcp::socket socket(ioc);
+    asio::connect(socket, endpoints);
+
+    {
+        std::string path = "/";
+        std::string request = "GET " + path + " HTTP/1.1\r\n";
+        request += std::string("") + "Host: " + "www.bilibili.com" + "\r\n";
+        request += "Accept: */*\r\n";
+        request += "Connection: close\r\n\r\n";
+
+        asio::write(socket, asio::buffer(request));
+
+        char buffer[1024] = {0};
+        asio::error_code ec;
+        while (size_t len = socket.read_some(asio::buffer(buffer), ec))
+        {
+            if (ec == asio::error::eof)
+            {
+                break;  // Connection closed cleanly by peer.
+            }
+            else if (ec)
+            {
+                throw asio::system_error(ec);  // Some other error.
+            }
+
+            std::cout.write(buffer, len);
+        }
+    }
+
+    return 0;
 }
